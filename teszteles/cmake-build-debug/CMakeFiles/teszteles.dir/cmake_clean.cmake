18ae51448b7410f1ac9ef90de31edf3d37fdc567
Tibor007 <79111062+Tibor007@users.noreply.github.com>
2021-02-16 16:14:28 +0200

    Probalgatas

---
+++ teszteles/cmake-build-debug/CMakeFiles/teszteles.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teszteles.dir/main.c.obj"
+  "libteszteles.dll.a"
+  "teszteles.exe"
+  "teszteles.exe.manifest"
+  "teszteles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/teszteles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
